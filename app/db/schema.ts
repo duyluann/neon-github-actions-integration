b752f024ba27327ae3853fde66a70449ab8ecd47
Luan Nguyen Duy <33739713+duyluann@users.noreply.github.com>
2024-12-16 18:24:51 +0700

    Update schema.ts
    feat: add created_at field to todo

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -4,4 +4,5 @@ export const todo = pgTable('todo', {
   id: integer('id').primaryKey(),
   text: text('text').notNull(),
   done: boolean('done').default(false).notNull(),
+  created_at: timestamp('created_at').notNull().defaultNow(),
 });
